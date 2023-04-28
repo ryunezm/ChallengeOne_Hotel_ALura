2249063349423ac7d17c2ca1f7b798ed3ff8a29f
ryunezm <61928963+ryunezm@users.noreply.github.com>
2023-04-28 13:50:33 -0500

    20230501: refactoring.

--- src/main/java/com/ryunezm/apps/hotelalura/enums/ListPaymentMethods.java
+++ src/main/java/com/ryunezm/apps/hotelalura/enums/ListPaymentMethods.java
@@ -1,17 +1,26 @@
 package com.ryunezm.apps.hotelalura.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum ListPaymentMethods {
 
-    mastercard("Mastercard", TypesPaymentMethods.card, 1.1),
-    visa("Visa", TypesPaymentMethods.card, 1.1),
-    american_express("American Express", TypesPaymentMethods.card, 1.1),
-    diners_club("Diners Club International", TypesPaymentMethods.card, 1.1),
-    paypal("PayPal", TypesPaymentMethods.online_payment, 1.01),
-    apple_pay("Apple Pay", TypesPaymentMethods.online_payment, 1.01),
-    google_pay("Google Pay", TypesPaymentMethods.online_payment, 1.01),
-    amazon_pay("Amazon Pay", TypesPaymentMethods.online_payment, 1.01),
+    mastercard("Mastercard", TypesPaymentMethods.card, 1.0),
+    visa("Visa", TypesPaymentMethods.card, 1.0),
+    american_express("American Express", TypesPaymentMethods.card, 1.0),
+    diners_club("Diners Club International", TypesPaymentMethods.card, 1.0),
+    paypal("PayPal", TypesPaymentMethods.online_payment, 1.0),
+    apple_pay("Apple Pay", TypesPaymentMethods.online_payment, 1.0),
+    google_pay("Google Pay", TypesPaymentMethods.online_payment, 1.0),
+    amazon_pay("Amazon Pay", TypesPaymentMethods.online_payment, 1.0),
     cash("Cash", TypesPaymentMethods.cash, 1.0);
 
+    private final String name;
+    private final TypesPaymentMethods typesPaymentMethods;
+    private final Double charge;
     ListPaymentMethods(String name, TypesPaymentMethods typesPaymentMethods, Double charge) {
+        this.name = name;
+        this.typesPaymentMethods = typesPaymentMethods;
+        this.charge = charge;
     }
 }
--- src/main/java/com/ryunezm/apps/hotelalura/model/Reserve.java
+++ src/main/java/com/ryunezm/apps/hotelalura/model/Reserve.java
@@ -1,66 +1,18 @@
 package com.ryunezm.apps.hotelalura.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.sql.Date;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class Reserve {
     private String reserveID;
     private Date checkInDate;
     private Date checkOutDate;
     private Double valueReserve;
-    private String paymentMethods;
-
-    public Reserve(String reserveID, Date checkInDate, Date checkOutDate, Double valueReserve, String paymentMethods) {
-        this.reserveID = reserveID;
-        this.checkInDate = checkInDate;
-        this.checkOutDate = checkOutDate;
-        this.valueReserve = valueReserve;
-        this.paymentMethods = paymentMethods;
-    }
-
-    public Reserve(Date checkInDate, Date checkOutDate, Double valueReserve, String paymentMethods) {
-        this.checkInDate = checkInDate;
-        this.checkOutDate = checkOutDate;
-        this.valueReserve = valueReserve;
-        this.paymentMethods = paymentMethods;
-    }
-
-    public String getReserveID() {
-        return reserveID;
-    }
-
-    public void setReserveID(String reserveID) {
-        this.reserveID = reserveID;
-    }
-
-    public Date getCheckInDate() {
-        return checkInDate;
-    }
-
-    public void setCheckInDate(Date checkInDate) {
-        this.checkInDate = checkInDate;
-    }
-
-    public Date getCheckOutDate() {
-        return checkOutDate;
-    }
-
-    public void setCheckOutDate(Date checkOutDate) {
-        this.checkOutDate = checkOutDate;
-    }
-
-    public Double getValueReserve() {
-        return valueReserve;
-    }
-
-    public void setValueReserve(Double valueReserve) {
-        this.valueReserve = valueReserve;
-    }
-
-    public String getPaymentMethods() {
-        return paymentMethods;
-    }
-
-    public void setPaymentMethods(String paymentMethods) {
-        this.paymentMethods = paymentMethods;
-    }
+    private PaymentMethod paymentMethod;
 }
